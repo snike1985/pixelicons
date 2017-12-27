aad78378beaaa6bfe63c6698dd91c132ca459b2d
alexserga <snike1985@gmail.com>
2017-12-27 09:28:01 +0200

    changed js for menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -5,7 +5,7 @@ class Menu {
         this.menuWrap = obj.getElementsByClassName('menu__wrap')[0];
         this.btnOpen = document.getElementsByClassName('menu-open')[0];
         this.btnClose = obj.getElementsByClassName('menu__close')[0];
-        this.body = document.getElementsByTagName('body')[0];
+        this.scrollContainer = document.getElementsByTagName('html')[0];
         this.header = document.getElementsByClassName('site__header')[0];
         this.lastScrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
 
@@ -16,12 +16,6 @@ class Menu {
         window.addEventListener('scroll', () => {
             let curScroll = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
 
-            if ( curScroll > 0 ) {
-                this.header.classList.add('site__header_fixed');
-            } else {
-                this.header.classList.remove('site__header_fixed');
-            }
-
             if (curScroll > this.lastScrollTop){
                 this.header.classList.add('hide');
             } else {
@@ -29,20 +23,28 @@ class Menu {
             }
 
             this.lastScrollTop = curScroll;
+
+            if ( curScroll > 0 ) {
+                this.header.classList.add('site__header_fixed');
+            } else {
+                this.header.classList.remove('site__header_fixed');
+                this.header.classList.remove('hide');
+            }
+
         });
         this.obj.addEventListener('click', () => {
             this.obj.classList.remove('active');
-            this.body.classList.remove('menu-active');
+            this.scrollContainer.classList.remove('menu-active');
             return false;
         });
         this.btnOpen.addEventListener('click', () => {
             this.obj.classList.add('active');
-            this.body.classList.add('menu-active');
+            this.scrollContainer.classList.add('menu-active');
             return false;
         });
         this.btnClose.addEventListener('click', () => {
             this.obj.classList.remove('active');
-            this.body.classList.remove('menu-active');
+            this.scrollContainer.classList.remove('menu-active');
             return false;
         });
         this.menuWrap.addEventListener('click', (e) => {
